f8c96462436424a1cc76e0d3cd8ff1e6002878c1
Alvaro Munoz <alvaro.munoz@gmail.com>
2022-07-16 03:04:00 -0400

    new format, added functions

--- manga.user.js
+++ manga.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Kindle manga/comic beta web reader enhancements
 // @namespace    http://alvaromunoz.cl/
-// @version      1.0.4
+// @version      1.0.5
 // @description  Adds enhancements to the kindle manga/comic beta web reader
 // @author       Álvaro Muñoz
 // @match        https://read.amazon.com/manga/*
@@ -12,153 +12,128 @@
 // ==/UserScript==
 
 (function () {
-  "use strict";
+    'use strict';
 
-  const emojiDict = {
-    lr: "➡️",
-    rl: "⬅️",
-  };
+    const emojiDict = {
+        lr: '➡️',
+        rl: '⬅️',
+    };
 
-  //Get ID to save per book!
-  const regex = /\/(\w+)\?/;
-  const amazonKindleId = document
-    .querySelector('meta[property="og:url"]')
-    .content.match(regex)[1];
-  console.log(`amazon kindle id: ${amazonKindleId}`);
-
-  // Select the node that will be observed for mutations
-  const targetNode = document.querySelector("body");
-  // Options for the observer (which mutations to observe)
-  const config = { attributes: true, childList: true, subtree: true };
-  // Callback function to execute when mutations are observed
-  const callback = function (mutationList, observer) {
-    // Use traditional 'for loops' for IE 11
-    for (const mutation of mutationList) {
-      if (mutation.type === "childList") {
-        //console.log('A child node has been added or removed.');
-        for (let node of mutation.addedNodes) {
-          //console.log('A child node has been added or removed: ' + node.nodeName);
-          if (node.id === "readerHamburgerMenu") {
-            if (
-              typeof GM_getValue(amazonKindleId + "navigationDirection") ===
-              "undefined"
-            ) {
-              GM_setValue(
-                amazonKindleId + "navigationDirection",
-                JSON.parse(document.getElementById("bookInfo").text)
-                  .contentMetadata.navigationDirection
-              );
-            }
-            const currentDirection = GM_getValue(
-              amazonKindleId + "navigationDirection"
-            );
-            const reverseDirection = currentDirection
-              .split("")
-              .reverse()
-              .join("");
-
-            let kindle_menuNode = document.getElementById(
-              "readerHamburgerMenuContent"
-            );
-
-            // SECTION
-            const extended_menuSectionInnerText =
-              document.createTextNode(`Extended Menu`);
-            const extended_menuSectionInner = document.createElement("div");
-            extended_menuSectionInner.setAttribute(
-              "class",
-              "kw-rd-hamburger-section-inner"
-            );
-            extended_menuSectionInner.append(extended_menuSectionInnerText);
-
-            const extended_menuSection = document.createElement("div");
-            extended_menuSection.setAttribute(
-              "class",
-              "kw-row kw-rd-hamburger-section"
-            );
-            extended_menuSection.append(extended_menuSectionInner);
-
-            //LIST
-            const extended_menuItemsList = document.createElement("ul");
-            extended_menuItemsList.setAttribute(
-              "class",
-              "kw-rd-hamburger-items-list"
-            );
-
-            //ITEM
-            const extended_menuItemText = document.createTextNode(
-              `Switch reading direction to ${emojiDict[reverseDirection]}`
-            );
-
-            const extended_menuItemLink = document.createElement("a");
-            extended_menuItemLink.setAttribute("class", "kw-text-normal");
-            extended_menuItemLink.setAttribute("href", "#");
-            extended_menuItemLink.setAttribute(
-              "id",
-              "readerHamburgerSwitchDirection"
-            );
-            extended_menuItemLink.addEventListener(
-              "click",
-              function (event) {
-                console.log(`Switching direction! to ${reverseDirection}`);
-                GM_setValue(
-                  amazonKindleId + "navigationDirection",
-                  reverseDirection
-                );
-                window.location.reload(true);
-              },
-              false
-            );
-            extended_menuItemLink.appendChild(extended_menuItemText);
-
-            const extended_menuItemInner = document.createElement("div");
-            extended_menuItemInner.setAttribute(
-              "class",
-              "kw-rd-hamburger-item-inner"
-            );
-            extended_menuItemInner.appendChild(extended_menuItemLink);
-
-            const extended_menuSpan = document.createElement("span");
-            extended_menuSpan.setAttribute("class", "kw-list-item");
-            extended_menuSpan.appendChild(extended_menuItemInner);
-
-            const extended_menuItem = document.createElement("li");
-            extended_menuItem.setAttribute("class", "kw-rd-hamburger-item");
-            extended_menuItem.appendChild(extended_menuSpan);
-
-            //add item to list
-            extended_menuItemsList.appendChild(extended_menuItem);
-
-            //add section and list to end of menu
-            kindle_menuNode.appendChild(extended_menuSection);
-            kindle_menuNode.appendChild(extended_menuItemsList);
-          }
-
-          // Modify bookInfo
-          if (node.id === "bookInfo") {
-            let bookInfoJson = JSON.parse(node.text);
-
-            if (
-              typeof GM_getValue(amazonKindleId + "navigationDirection") !==
-              "undefined"
-            ) {
-              bookInfoJson.contentMetadata.navigationDirection = GM_getValue(
-                amazonKindleId + "navigationDirection"
-              );
-              node.text = JSON.stringify(bookInfoJson);
-              console.log(
-                `kindle navigation Direction set to : ${bookInfoJson.contentMetadata.navigationDirection}`
-              );
-            }
-          }
-        }
-      }
+    function createMenuItem(menuText, menuId, func) {
+        let extended_menuItemText = document.createTextNode(menuText);
+        let extended_menuItemLink = document.createElement('a');
+        extended_menuItemLink.setAttribute('class', 'kw-text-normal');
+        extended_menuItemLink.setAttribute('href', '#');
+        extended_menuItemLink.setAttribute('id', menuId);
+        extended_menuItemLink.addEventListener('click', func, false);
+        extended_menuItemLink.appendChild(extended_menuItemText);
+        let extended_menuItemInner = document.createElement('div');
+        extended_menuItemInner.setAttribute('class', 'kw-rd-hamburger-item-inner');
+        extended_menuItemInner.appendChild(extended_menuItemLink);
+        let extended_menuSpan = document.createElement('span');
+        extended_menuSpan.setAttribute('class', 'kw-list-item');
+        extended_menuSpan.appendChild(extended_menuItemInner);
+        let extended_menuItem = document.createElement('li');
+        extended_menuItem.setAttribute('class', 'kw-rd-hamburger-item');
+        extended_menuItem.appendChild(extended_menuSpan);
+        return extended_menuItem;
     }
-  };
 
-  // Create an observer instance linked to the callback function
-  const observer = new MutationObserver(callback);
+    function createMenuSection(sectionText) {
+        let extended_menuSectionInnerText = document.createTextNode(sectionText);
+        let extended_menuSectionInner = document.createElement('div');
+        extended_menuSectionInner.setAttribute('class', 'kw-rd-hamburger-section-inner');
+        extended_menuSectionInner.append(extended_menuSectionInnerText);
+        let extended_menuSection = document.createElement('div');
+        extended_menuSection.setAttribute('class', 'kw-row kw-rd-hamburger-section');
+        extended_menuSection.append(extended_menuSectionInner);
+        return extended_menuSection;
+    }
 
-  // Start observing the target node for configured mutations
-  observer.observe(targetNode, config);
+    function createMenuList() {
+        let extended_menuItemsList = document.createElement('ul');
+        extended_menuItemsList.setAttribute('class', 'kw-rd-hamburger-items-list');
+        return extended_menuItemsList;
+    }
+
+    //Get ID to save per book!
+    const regex = /\/(\w+)\?/;
+    const amazonKindleId = document.querySelector('meta[property="og:url"]').content.match(regex)[1];
+    console.log(`amazon kindle id: ${amazonKindleId}`);
+
+    // Select the node that will be observed for mutations
+    const targetNode = document.querySelector('body');
+    // Options for the observer (which mutations to observe)
+    const config = { attributes: true, childList: true, subtree: true };
+    // Callback function to execute when mutations are observed
+    const callback = function (mutationList, observer) {
+        // Use traditional 'for loops' for IE 11
+        for (const mutation of mutationList) {
+            if (mutation.type === 'childList') {
+                // console.log('A child node has been added or removed.');
+                for (let node of mutation.addedNodes) {
+                    // console.log('A child node has been added or removed: ' + node.nodeName);
+
+                    // Edit Hamburger Menu
+                    if (node.id === 'readerHamburgerMenu') {
+                        // Get amazon book ID
+                        if (typeof GM_getValue(amazonKindleId + 'navigationDirection') === 'undefined') {
+                            GM_setValue(
+                                amazonKindleId + 'navigationDirection',
+                                JSON.parse(document.getElementById('bookInfo').text).contentMetadata.navigationDirection
+                            );
+                        }
+
+                        // Set reading directions variables
+                        let currentDirection = GM_getValue(amazonKindleId + 'navigationDirection');
+                        let reverseDirection = currentDirection.split('').reverse().join('');
+
+                        // Create menuNode
+                        let kindle_menuNode = document.getElementById('readerHamburgerMenuContent');
+
+                        // Create Section
+                        let extended_menuSection = createMenuSection('EXTENDED MENU');
+
+                        // Create List
+                        let extended_menuItemsList = createMenuList();
+
+                        // Create Item
+                        let extended_menuItem = createMenuItem(
+                            `Switch reading direction to ${emojiDict[reverseDirection]}`,
+                            'readerHamburgerSwitchDirection',
+                            function (event) {
+                                console.log(`Switching direction! to ${reverseDirection}`);
+                                GM_setValue(amazonKindleId + 'navigationDirection', reverseDirection);
+                                window.location.reload(true);
+                            }
+                        );
+
+                        // Add item to list
+                        extended_menuItemsList.appendChild(extended_menuItem);
+
+                        // Add section and list to end of menu
+                        kindle_menuNode.appendChild(extended_menuSection);
+                        kindle_menuNode.appendChild(extended_menuItemsList);
+                    }
+
+                    // Modify bookInfo
+                    if (node.id === 'bookInfo') {
+                        let bookInfoJson = JSON.parse(node.text);
+
+                        // Change navigation direction to saved value
+                        if (typeof GM_getValue(amazonKindleId + 'navigationDirection') !== 'undefined') {
+                            bookInfoJson.contentMetadata.navigationDirection = GM_getValue(amazonKindleId + 'navigationDirection');
+                            node.text = JSON.stringify(bookInfoJson);
+                        }
+                    }
+                }
+            }
+        }
+    };
+
+    // Create an observer instance linked to the callback function
+    const observer = new MutationObserver(callback);
+
+    // Start observing the target node for configured mutations
+    observer.observe(targetNode, config);
 })();
